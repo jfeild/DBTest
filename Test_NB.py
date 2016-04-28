730122e030d66a4d29486e4222df3033aaf24f82
jfeild <jacqueline.feild@gmail.com>
2016-04-28 11:22:51 -0400

    Update Test_NB.py

---
+++ Test_NB.py
@@ -0,0 +1,15 @@
+# Databricks notebook source exported at Thu, 28 Apr 2016 15:22:53 UTC
+import pandas as pd
+
+# COMMAND ----------
+
+a = 1
+b = 2
+c = a+b
+
+# COMMAND ----------
+
+print c
+
+# COMMAND ----------
+
